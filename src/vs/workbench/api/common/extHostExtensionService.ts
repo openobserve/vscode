cf6b10a0d27c3ad7e911b9e1e7a467f270eb2aa0
omkark06 <omkar@zinclabs.io>
2025-03-11 16:22:05 +0530

    fix: added action-id to workspace state to access in extension context

--- src/vs/workbench/api/common/extHostExtensionService.ts
+++ src/vs/workbench/api/common/extHostExtensionService.ts
@@ -522,6 +522,8 @@ export abstract class AbstractExtHostExtensionService extends Disposable impleme
 				? this._initData.messagePorts?.get(ExtensionIdentifier.toKey(extensionDescription.identifier))
 				: undefined;
 
+			workspaceState.update('actionId', this._initData.workspace?.actionId);
+
 			return Object.freeze<vscode.ExtensionContext>({
 				globalState,
 				workspaceState,
